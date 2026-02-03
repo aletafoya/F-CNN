71f4338f293eea1e7eba00f78dcc53d878656c0b
aletafoya <ale.tufk@gmail.com>
2026-02-02 19:22:38 -0600

    feature: images for vm

---
+++ mbl/app/src/main/java/com/example/forgotten/ss_screen.kt
@@ -0,0 +1,19 @@
+package com.example.forgotten
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import android.widget.Toast
+
+
+
+@Composable
+fun screen(modifier: Modifier) {
+    Card(modifier = modifier.fillMaxWidth()
+        .fillMaxHeight()
+    ) {
+
+    }
+}
